b02ef1a1af43b762c1314eafd1df1875f04f3c8f
Jeremias Erbs <jerry@badabam.de>
2017-09-01 16:08:30 +0200

    Add fadeout of instructions

--- js/app-instructions.js
+++ js/app-instructions.js
@@ -3,10 +3,15 @@ new Vue({
   template: `
   <aside
     class="instructions"
+    v-bind:class="{fadeout: fadeout}"
     v-html="i18n.instructions.htmlText">
   </aside>
   `,
   data: {
-    i18n: i18n
+    i18n: i18n,
+    fadeout: false
+  },
+  mounted() {
+    setTimeout(() => this.fadeout = true);
   }
 });
