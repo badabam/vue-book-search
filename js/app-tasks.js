ad59b7f2197730be88dded378a9d7534b3d11046
Jeremias Erbs <jerry@badabam.de>
2017-09-04 12:04:35 +0200

    Update tasks

--- js/app-tasks.js
+++ js/app-tasks.js
@@ -4,7 +4,7 @@ new Vue({
     <section class="app-tasks">
     <h3>Aufgaben:</h3>
       <ol>
-        <li>Ich suche ein Buch von Richard David Precht. Irgendwas mit Liebe.</li>
+        <li>Ich suche ein Buch von "Richard David Precht". Irgendwas mit "Liebe".</li>
         <li>Haben Sie etwas zum Thema "Kartenkunst" oder "Spielkarten"?</li>
         <li>Ich suche eine besonders schöne Bibel auf französisch oder italienisch als Geschenk. Mindestens 200 Euro wert.</li>
         <li>Ich suche die 'Herr der Ringe' BluRay Special Edition. Ist letztes Jahr erst erschienen.</li>
